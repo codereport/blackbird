9fca4196f529638efea124cfeb56a86e8d8cf92a
Conor Hoekstra <codereport@outlook.com>
2022-07-06 10:02:20 -0400

    B, Phi & more ops

--- combinators.hpp
+++ combinators.hpp
@@ -5,15 +5,32 @@
 
 namespace combinators {
 
-// combinators
+/////////////////
+// combinators //
+/////////////////
 
-// Psi (The Blackbird)
+// B (The Bluebird)
+auto _b = [](auto f, auto g) { //
+  return [=](auto x) { return f(g(x)); };
+};
+
+// Phi (The Phoenix)
+auto _phi = [](auto f, auto g, auto h) {
+  return [=](auto x) { return g(f(x), h(x)); };
+};
+
+// Psi (The Psi Bird)
 auto _psi = [](auto f, auto g) {
   return [=](auto x, auto y) { return f(g(x), g(y)); };
 };
 
-// more convenient binary operations
+/////////////////////////////////////////////
+// more convenient binary/unary operations //
+/////////////////////////////////////////////
 
+auto _eq = [](auto x) { return [x](auto y) { return x == 'y'; }; };
+auto _eq_ = std::equal_to{};
+auto _or = std::logical_or{};
 auto _and = std::logical_and{};
 
 } // namespace combinators

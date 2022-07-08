40fa017bcd062f050c682a937db2c74c96078cae
Conor Hoekstra <codereport@outlook.com>
2022-07-08 16:45:19 -0400

    Add _plus(_) and fix or/and spelling

--- combinators.hpp
+++ combinators.hpp
@@ -10,27 +10,23 @@ namespace combinators {
 /////////////////
 
 // B (The Bluebird)
-auto _b = [](auto f, auto g) { //
-  return [=](auto x) { return f(g(x)); };
-};
+auto _b = [](auto f, auto g) { return [=](auto x) { return f(g(x)); }; };
 
 // Phi (The Phoenix)
-auto _phi = [](auto f, auto g, auto h) {
-  return [=](auto x) { return g(f(x), h(x)); };
-};
+auto _phi = [](auto f, auto g, auto h) { return [=](auto x) { return g(f(x), h(x)); }; };
 
 // Psi (The Psi Bird)
-auto _psi = [](auto f, auto g) {
-  return [=](auto x, auto y) { return f(g(x), g(y)); };
-};
+auto _psi = [](auto f, auto g) { return [=](auto x, auto y) { return f(g(x), g(y)); }; };
 
 /////////////////////////////////////////////
 // more convenient binary/unary operations //
 /////////////////////////////////////////////
 
-auto _eq = [](auto x) { return [x](auto y) { return x == y; }; };
-auto _eq_ = std::equal_to{};
-auto _or = std::logical_or{};
-auto _and = std::logical_and{};
+auto _eq    = [](auto x) { return [x](auto y) { return x == y; }; };
+auto _eq_   = std::equal_to{};
+auto _plus  = [](auto x) { return [x](auto y) { return x + y; }; };
+auto _plus_ = std::plus{};
+auto _or_   = std::logical_or{};
+auto _and_  = std::logical_and{};
 
-} // namespace combinators
+}  // namespace combinators
